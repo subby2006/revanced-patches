d99af748ad3d10519ceffebdb453e83e3591e269
inotia00 <ars258000@gmail.com>
2022-10-03 18:20:25 +0900

    Deprecated support for YouTube v17.38.35, added support for YouTube v17.38.36

--- src/main/kotlin/app/revanced/patches/youtube/extended/speed/annotations/DefaultVideoSpeedCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/youtube/extended/speed/annotations/DefaultVideoSpeedCompatibility.kt
@@ -4,9 +4,9 @@ import app.revanced.patcher.annotation.Compatibility
 import app.revanced.patcher.annotation.Package
 
 @Compatibility(
-        [Package(
-                "com.google.android.youtube", arrayOf("17.32.39", "17.34.36", "17.36.39", "17.37.35", "17.38.35")
-        )]
+    [Package(
+        "com.google.android.youtube", arrayOf("17.32.39", "17.34.36", "17.36.39", "17.37.35", "17.38.36")
+    )]
 )
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
