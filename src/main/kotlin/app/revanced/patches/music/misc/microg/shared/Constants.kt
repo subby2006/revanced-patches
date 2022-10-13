ba157f65b836a924cf64101bd1b3a7b6b6e43ae1
inotia00 <ars258000@gmail.com>
2022-10-14 02:56:38 +0900

    Add missing constants

--- src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
+++ src/main/kotlin/app/revanced/patches/music/misc/microg/shared/Constants.kt
@@ -2,6 +2,7 @@ package app.revanced.patches.music.misc.microg.shared
 
 object Constants {
     internal const val REVANCED_MUSIC_PACKAGE_NAME = "app.revanced.android.apps.youtube.music"
+    internal const val REVANCED_EXTENDED_MUSIC_PACKAGE_NAME = "app.rvx.android.apps.youtube.music"
     internal const val MUSIC_PACKAGE_NAME = "com.google.android.apps.youtube.music"
     internal const val SPOOFED_PACKAGE_NAME = MUSIC_PACKAGE_NAME
     internal const val SPOOFED_PACKAGE_SIGNATURE = "afb0fed5eeaebdd86f56a97742f4b6b33ef59875"
