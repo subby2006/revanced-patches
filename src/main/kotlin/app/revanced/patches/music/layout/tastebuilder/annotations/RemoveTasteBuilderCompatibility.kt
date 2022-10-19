027ef1058632788a9673d30fecba74ccc16e0a9b
inotia00 <ars258000@gmail.com>
2022-10-19 14:44:46 +0900

    Add support for YouTube Music v5.29.52

--- src/main/kotlin/app/revanced/patches/music/layout/tastebuilder/annotations/RemoveTasteBuilderCompatibility.kt
+++ src/main/kotlin/app/revanced/patches/music/layout/tastebuilder/annotations/RemoveTasteBuilderCompatibility.kt
@@ -10,7 +10,7 @@ import app.revanced.patcher.annotation.Package
  */
 @Compatibility(
     [Package(
-        "com.google.android.apps.youtube.music", arrayOf("5.23.50", "5.25.51", "5.26.53", "5.27.51", "5.28.51")
+        "com.google.android.apps.youtube.music", arrayOf("5.23.50", "5.27.51", "5.28.52", "5.29.52")
     )]
 )
 @Target(AnnotationTarget.CLASS)
