77de72b0568495079bf45ad9002f6d4192b91b20
inotia00 <ars258000@gmail.com>
2022-10-24 16:46:59 +0900

    bump v2.85.2

---
+++ src/main/kotlin/app/revanced/patches/youtube/misc/integrations/fingerprints/ServiceFingerprint.kt
@@ -0,0 +1,13 @@
+package app.revanced.patches.youtube.misc.integrations.fingerprints
+
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.extensions.YouTubeCompatibility
+
+@Name("service-fingerprint")
+@YouTubeCompatibility
+@Version("0.0.1")
+object ServiceFingerprint : MethodFingerprint(
+    customFingerprint = {  methodDef -> methodDef.definingClass.endsWith("ApiPlayerService;") && methodDef.name == "<init>" }
+)
