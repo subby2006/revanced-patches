060d3ef260bac3ddb0f7bf258337916e28ea538a
inotia00 <ars258000@gmail.com>
2022-11-10 21:11:03 +0900

    revert: feat(youtube/return-youtube-dislike): compatibility for old and new button layout

--- src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/RemoveLikeFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/RemoveLikeFingerprint.kt
@@ -1,17 +1,21 @@
 package app.revanced.patches.youtube.layout.returnyoutubedislike.fingerprints
 
-import app.revanced.annotation.YouTubeCompatibility
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.extensions.or
+import app.revanced.patcher.fingerprint.method.annotation.FuzzyPatternScanMethod
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
+import app.revanced.annotation.YouTubeCompatibility
 import org.jf.dexlib2.AccessFlags
 
 @Name("remove-like-fingerprint")
+@FuzzyPatternScanMethod(2)
 @YouTubeCompatibility
 @Version("0.0.2")
 object RemoveLikeFingerprint : MethodFingerprint(
     "V",
     AccessFlags.PROTECTED or AccessFlags.CONSTRUCTOR,
-    strings = listOf("like/removelike")
+    null,
+    null,
+    listOf("like/removelike")
 )
--- src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/TextComponentSpecParentFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/returnyoutubedislike/fingerprints/TextComponentSpecParentFingerprint.kt
@@ -1,14 +1,17 @@
 package app.revanced.patches.youtube.layout.returnyoutubedislike.fingerprints
 
-import app.revanced.annotation.YouTubeCompatibility
 import app.revanced.patcher.annotation.Name
 import app.revanced.patcher.annotation.Version
 import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
+import app.revanced.annotation.YouTubeCompatibility
 
 @Name("text-component-spec-parent-fingerprint")
 @YouTubeCompatibility
 @Version("0.0.1")
 object TextComponentSpecParentFingerprint : MethodFingerprint(
-    strings = listOf("TextComponentSpec: No converter for extension: ")
+    null,
+    null,
+    null,
+    null,
+    listOf("TextComponentSpec: No converter for extension: ")
 )
