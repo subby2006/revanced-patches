e5bbf31f758c24f728841b1a4a305272a70a56d4
inotia00 <ars258000@gmail.com>
2022-11-16 20:50:47 +0900

    Add `optimize-resource-music` patch

---
+++ src/main/kotlin/app/revanced/patches/music/misc/optimizeresource/patch/OptimizeResourcePatch.kt
@@ -0,0 +1,37 @@
+package app.revanced.patches.music.misc.optimizeresource.patch
+
+import app.revanced.patcher.annotation.Description
+import app.revanced.patcher.annotation.Name
+import app.revanced.patcher.annotation.Version
+import app.revanced.patcher.data.ResourceContext
+import app.revanced.patcher.patch.PatchResult
+import app.revanced.patcher.patch.PatchResultSuccess
+import app.revanced.patcher.patch.annotations.Patch
+import app.revanced.patcher.patch.ResourcePatch
+import app.revanced.annotation.YouTubeMusicCompatibility
+import app.revanced.util.resources.ResourceUtils.copyXmlNode
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.Comparator
+import java.nio.file.StandardCopyOption
+
+@Patch
+@Name("optimize-resource-music")
+@Description("Remove unnecessary resources.")
+@YouTubeMusicCompatibility
+@Version("0.0.1")
+class OptimizeResourcePatch : ResourcePatch {
+    override fun execute(context: ResourceContext): PatchResult {
+
+        val relativePath = "raw/third_party_licenses"
+
+        Files.copy(
+            this.javaClass.classLoader.getResourceAsStream("youtube/resource/$relativePath")!!,
+            context["res"].resolve(relativePath).toPath(),
+            StandardCopyOption.REPLACE_EXISTING
+        )
+        
+        return PatchResultSuccess()
+    }
+}
